3eeb4a810d3aff33f9114ddc5d6ac7eeee0af6cf
Denis Bobrov <delneg@yandex.ru>
2021-10-23 10:42:43 +0300

    Remove the rest of paket, fix Fsharp.core version for Fable.Python, move references from paket for Fable.Python.Test.fsproj, start django example

---
+++ examples/django/Program.fs
@@ -0,0 +1,10 @@
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+module Program
+
+open System
+open Fable.Python
+
+[<EntryPoint>]
+let main argv =
+    printfn "Started ..."
+    0 // return an integer exit code
