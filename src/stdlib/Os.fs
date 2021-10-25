b5070df1f637549e7fab05174e4f8c9856c690a2
Dag Brattli <dag@brattli.net>
2021-10-25 21:09:54 +0200

    Update for os

--- src/stdlib/Os.fs
+++ src/stdlib/Os.fs
@@ -6,9 +6,14 @@ open Fable.Core
 // fsharplint:disable MemberNames
 
 type IExports =
+    abstract chdir : string -> unit
+    abstract chroot : string -> unit
+    abstract close : fd: int -> unit
     abstract environ : Dictionary<string, string>
+    abstract getcwd : unit -> string
     abstract getenv : key: string -> string option
     abstract getenv : key: string * ``default``: string -> string
+    abstract kill : pid: int * ``sig``: int -> unit
     abstract putenv : key: string * value: string -> unit
 
 
