8aa3d435f1f471fad868671b2b4cff752e23aac0
Dag Brattli <dag@brattli.net>
2021-11-14 22:25:04 +0100

    WIP

--- src/stdlib/asyncio/Tasks.fs
+++ src/stdlib/asyncio/Tasks.fs
@@ -1,17 +1,28 @@
-module Fable.Python.AsyncIO.Tasks
+namespace Fable.Python.AsyncIO
 
 open Fable.Core
 
 type Task<'T> =
     inherit Future<'T>
 
-type Coroutine = interface end
+type Coroutine<'T> =
+    inherit Awaitable<'T>
+
+    abstract member send: 'T -> unit
+    abstract member close: unit -> unit
+    abstract member throw: exn -> unit
 
 
 type IExports =
-    abstract create_task<'T> : fn: Coroutine -> Task<'T>
+    abstract create_task<'T> : fn: Coroutine<'T> -> Task<'T>
     abstract sleep : seconds: float -> result: 'T -> Future<'T>
     abstract run : main: Awaitable<'T> -> 'T
+    [<Emit("$0.get_running_loop()")>]
+    abstract get_running_loop : unit -> AbstractEventLoop
+    [<Emit("$0.get_event_loop()")>]
+    abstract get_event_loop : unit -> AbstractEventLoop
 
-[<ImportAll("asyncio")>]
-let asyncio: IExports = nativeOnly
+[<AutoOpen>]
+module AsyncIOImpl =
+    [<ImportAll("asyncio")>]
+    let asyncio: IExports = nativeOnly
