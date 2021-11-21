9510985362a13281c1a08d9b235748a3b9ec5773
Dag Brattli <dag@brattli.net>
2021-11-21 11:58:35 +0100

    Align with tasks in Fable compiler

--- src/stdlib/asyncio/Tasks.fs
+++ src/stdlib/asyncio/Tasks.fs
@@ -16,7 +16,8 @@ type Coroutine<'T> =
 type IExports =
     abstract create_task<'T> : fn: Coroutine<'T> -> Task<'T>
     abstract sleep : seconds: float -> result: 'T -> Future<'T>
-    abstract run : main: Awaitable<'T> -> 'T
+    abstract run<'T> : main: Awaitable<'T> -> 'T
+    abstract run<'T> : main: System.Threading.Tasks.Task<'T> -> 'T
     [<Emit("$0.get_running_loop()")>]
     abstract get_running_loop : unit -> AbstractEventLoop
     [<Emit("$0.get_event_loop()")>]
