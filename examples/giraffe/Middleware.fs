93cb6a23dbc0b08bcd0127b80a356752414e3668
Dag Brattli <dag@brattli.net>
2022-09-05 08:26:51 +0200

    Fix default handler

--- examples/giraffe/Middleware.fs
+++ examples/giraffe/Middleware.fs
@@ -8,13 +8,19 @@ type App = Func<Scope, unit -> Task<Response>, Request -> Task<unit>, Task<unit>
 module Middleware =
     let useGiraffe (handler: HttpHandler) : App =
         let func: HttpFunc = handler earlyReturn
+        let defaultHandler = setStatusCode 404 >=> text ""
+        let defaultFunc: HttpFunc = defaultHandler earlyReturn
 
         let app (scope: Scope) (receive: unit -> Task<Response>) (send: Request -> Task<unit>) =
             task {
-                printfn "Scope %A" scope
+                //printfn "Scope %A" scope
                 let ctx = HttpContext(scope, receive, send)
                 let! result = func ctx
-                ()
+                match result with
+                | None ->
+                    let! _ = defaultFunc ctx
+                    ()
+                | _ -> ()
             }
 
         Func<_, _, _, _>(app)
