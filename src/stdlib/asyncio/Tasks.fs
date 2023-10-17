a590bd0176fbc7acbc6d7cae7464257f1fbae9f6
Dag Brattli <dag@brattli.net>
2023-10-17 09:29:38 +0200

    Prepare timeflies for the new asyncio mainloop

--- src/stdlib/asyncio/Tasks.fs
+++ src/stdlib/asyncio/Tasks.fs
@@ -5,6 +5,9 @@ open Fable.Core
 type Task<'T> =
     inherit Future<'T>
 
+type Task =
+    inherit Future<unit>
+
 type Coroutine<'T> =
     inherit Awaitable<'T>
 
@@ -15,7 +18,12 @@ type Coroutine<'T> =
 
 type IExports =
     abstract create_task<'T> : fn: Coroutine<'T> -> Task<'T>
-    abstract sleep: seconds: float -> result: 'T -> Future<'T>
+    /// Translates a Python Task to a System.Threading.Tasks.Task
+    abstract create_task: task: Task -> System.Threading.Tasks.Task
+    /// Translates a Python Task<'T> to a System.Threading.Tasks.Task<'T>
+    abstract create_task: task: Task<'T> -> System.Threading.Tasks.Task<'T>
+    abstract sleep: seconds: float -> Task
+    abstract sleep: seconds: float * result: 'T -> Task<'T>
     abstract run<'T> : main: Awaitable<'T> -> 'T
     abstract run<'T> : main: System.Threading.Tasks.Task<'T> -> 'T
 
--- test/TestAsyncIO.fs
+++ test/TestAsyncIO.fs
@@ -19,3 +19,19 @@ let ``test builder run int works`` () =
     let result = asyncio.run(tsk)
     result |> equal 42
 
+[<Fact>]
+let ``test sleep works`` () =
+    let tsk = task {
+        do! asyncio.create_task(asyncio.sleep(0.1))
+        return 42
+    }
+    let result = asyncio.run(tsk)
+    result |> equal 42
+
+[<Fact>]
+let ``test sleep with value works`` () =
+    let tsk = task {
+        return! asyncio.create_task(asyncio.sleep(0.1, 42))
+    }
+    let result = asyncio.run(tsk)
+    result |> equal 42
