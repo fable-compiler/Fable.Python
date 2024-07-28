9c98d503a718926bdf9808430491f206fdd05a4a
Joseph Price <pricejosephd@gmail.com>
2024-07-28 09:26:01 -0400

    add sys.exit

--- src/stdlib/Sys.fs
+++ src/stdlib/Sys.fs
@@ -28,6 +28,15 @@ type IExports =
     abstract prefix: string
     abstract version: string
     abstract version_info: VersionInfo
+    /// Exits with code 0, indicating success
+    /// See https://docs.python.org/3/library/sys.html#sys.exit
+    abstract exit: unit -> 'a
+    /// Exits with provided status
+    /// See https://docs.python.org/3/library/sys.html#sys.exit
+    abstract exit: status: int -> 'a
+    /// Exits with exit status 1, printing message to stderr
+    /// See https://docs.python.org/3/library/sys.html#sys.exit
+    abstract exit: message: string -> 'a
 
 /// System-specific parameters and functions
 [<ImportAll("sys")>]
